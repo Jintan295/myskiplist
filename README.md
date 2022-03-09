55b1bc91e577c2e3de413bd203c5cc1e50f2e953
wangyifan <759051709@qq.com>
2022-03-09 16:30:23 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myskiplist
+
+
+
+## 学习项目，原作者是carl，引用 https://github.com/youngyangyang04/Skiplist-CPP
--- skiplist.h
+++ skiplist.h
@@ -1,6 +1,3 @@
-/******************
-  Author :   wangyifan
-****************/
 
 
 #include <iostream>
